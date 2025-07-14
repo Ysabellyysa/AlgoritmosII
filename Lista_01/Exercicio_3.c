05936191affe5be87dfcf413c4e8cbc2f7be71a6
MaryaYsabella <maryaysabellacolatino17@gmail.com>
2025-07-14 13:14:58 -0300

    Create Exercicio_3.c
    Construa um programa em C usando switch/case para ler a letra do estado civil de
    uma pessoa e mostrar uma mensagem para indicar um local para pessoa ir no final de
    semana. Considere: 'c' para casado, 's' para solteiro, 'v' para viúvo e 'd' para divorciado. Para
    cada uma das entradas, as saídas devem ser:
    c – Indicação: Caramel Sweet & Salts
    – Indicação: London Pub
    v – Indicação: XI Baile da Melhor Idade
    d – Indicação: Academia Smart FiT

---
+++ Lista_01/Exercicio_3.c
@@ -0,0 +1,20 @@
+printf("Estado civil (C,S,V ou D):");
+scanf("%c", &opcao);
+
+switch(opcao){
+case 'C':
+    printf("Indicacao: Caramel Sweet & Salt");
+    break;
+case 'S':
+    printf("Indicacao: London Pub");
+    break;
+case 'V':
+    printf("Indicacao: XI Baile da Melhor Idade");
+    break;
+case 'D':
+    printf("Indicacao: Academia Smart Fit");
+    break;
+default:
+    printf("Erro!");
+
+}
