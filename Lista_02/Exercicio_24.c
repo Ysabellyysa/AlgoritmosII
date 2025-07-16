98a8e79cfc59a85d013f1599217a66432240c346
MaryaYsabella <maryaysabellacolatino17@gmail.com>
2025-07-15 21:16:37 -0300

    Create Exercicio_24.c
    Escreva uma função que gera um triângulo de altura e lados n e base 2*n-1. Por
    exemplo, a saída para n = 6 seria:

---
+++ Lista_02/Exercicio_24.c
@@ -0,0 +1,25 @@
+
+void triangulo(int n) {
+    for (int i = 1; i <= n; i++) {
+        int espacos = n - i;
+        int asteriscos = 2 * i - 1;
+
+        for (int j = 0; j < espacos; j++) {
+            printf(" ");
+        }
+        for (int j = 0; j < asteriscos; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int n;
+    printf("Digite um valor: ");
+    scanf("%d", &n);
+
+    triangulo(n);
+
+    return 0;
+}
