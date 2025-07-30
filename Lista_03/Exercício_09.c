c188ee2b27d20adfefeb394fcac3d9122d003578
MaryaYsabella <maryaysabellacolatino17@gmail.com>
2025-07-30 15:53:33 -0300

    Create Exercício_09.c
    Crie um programa que contenha uma matriz de float contendo 3 linhas e 3 colunas. Imprima o
    endereço de cada posição dessa matriz.

---
+++ Lista_03/Exercício_09.c
@@ -0,0 +1,12 @@
+int main(){
+   float matriz[3][3];
+
+   for(int i=0;i<3;i++){
+     for(int j=0;j<3;j++){
+
+        printf("Endereco da matriz[%d][%d]: %p\n", i,j,&matriz[i][j]);
+     }
+   }
+
+   return 0;
+}
